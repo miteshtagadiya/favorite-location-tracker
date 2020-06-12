7665afadebd746206ac4dd27f6ead771e50840cc
mitesh <miteshtagadiya@gmail.com>
2020-06-12 22:09:06 +0530

    Feature: Create Header Component

---
+++ src/Components/Ui/Header/Header.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import { AccountCircle } from "@material-ui/icons";
+import { Menu, Divider, MenuItem } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  toolbar: {
+    paddingRight: 24,
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+}));
+
+const Header = (props) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <div className={classes.profileMenu}>
+        <Typography
+          style={{ padding: 15, fontWeight: "bold" }}
+          variant="inherit"
+          noWrap
+        >
+          {props.user.email}
+        </Typography>
+      </div>
+      <Divider style={{ marginTop: 20 }} />
+
+      <MenuItem onClick={() => {}}>Logout</MenuItem>
+    </Menu>
+  );
+
+  return (
+    <>
+      <AppBar position="absolute" className={clsx(classes.appBar)}>
+        <Toolbar className={classes.toolbar}>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            className={classes.title}
+          >
+            Favourite Location Tracker
+          </Typography>
+          <IconButton onClick={handleProfileMenuOpen} color="inherit">
+            <AccountCircle />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </>
+  );
+};
+
+export default Header;

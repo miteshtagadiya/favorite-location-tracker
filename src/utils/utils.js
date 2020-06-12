6288ee4ba13bd7ee1cd156791abdd5d810f9af1d
mitesh <miteshtagadiya@gmail.com>
2020-06-13 00:53:00 +0530

    Fix: linting

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,21 +1,20 @@
 export const degreesToRadians = (degrees) => {
-    return (degrees * Math.PI) / 180;
-  };
-  
-  export const getDistanceBetweenPoints = (lat1, lng1, lat2, lng2) => {
-    let R = 6378137;
-    let dLat = degreesToRadians(lat2 - lat1);
-    let dLong = degreesToRadians(lng2 - lng1);
-    let a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+  return (degrees * Math.PI) / 180;
+};
+
+export const getDistanceBetweenPoints = (lat1, lng1, lat2, lng2) => {
+  let R = 6378137;
+  let dLat = degreesToRadians(lat2 - lat1);
+  let dLong = degreesToRadians(lng2 - lng1);
+  let a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(degreesToRadians(lat1)) *
       Math.cos(degreesToRadians(lat1)) *
-        Math.cos(degreesToRadians(lat1)) *
-        Math.sin(dLong / 2) *
-        Math.sin(dLong / 2);
-  
-    let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    let distance = R * c;
-  
-    return distance;
-  };
-  
+      Math.sin(dLong / 2) *
+      Math.sin(dLong / 2);
+
+  let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  let distance = R * c;
+
+  return distance;
+};

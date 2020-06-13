2898341abc1d5119c241f8aecdddf10145f34b6e
mitesh <miteshtagadiya@gmail.com>
2020-06-13 10:14:26 +0530

    Feature: Add routes for login, signup and places

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { auth } from "./services/firebase";
+import Home from "./components/App/Home/Home";
+import SignUp from "./components/App/SignUp/SignUp";
+import SignIn from "./components/App/SignIn/SignIn";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      authenticated: false,
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false,
+        });
+      } else {
+        this.setState({
+          authenticated: false,
+          loading: false,
+        });
+      }
+    });
+  }
+
+  render() {
+    return this.state.loading === true ? (
+      <div className="spinner-border text-success" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    ) : (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route
+            path="/places"
+            render={(props) =>
+              this.state.authenticated === true ? (
+                <Home {...props} />
+              ) : (
+                <Redirect
+                  to={{ pathname: "/login", state: { from: props.location } }}
+                />
+              )
+            }
+          />
+          <Route
+            path="/signup"
+            render={(props) =>
+              this.state.authenticated === false ? (
+                <SignUp {...props} />
+              ) : (
+                <Redirect to="/places" />
+              )
+            }
+          />
+          <Route
+            path="/login"
+            render={(props) =>
+              this.state.authenticated === false ? (
+                <SignIn {...props} />
+              ) : (
+                <Redirect to="/places" />
+              )
+            }
+          />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;

20afa36f0edbda46995e19f97d6f85196ba72297
mitesh <miteshtagadiya@gmail.com>
2020-06-13 10:17:47 +0530

    Refacture: SignIn by firebase

--- src/components/App/SignIn/SignIn.jsx
+++ src/components/App/SignIn/SignIn.jsx
@@ -5,11 +5,11 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
 import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import { signin } from "../../../helpers/auth";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -44,6 +44,12 @@ export default function SignIn() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setError("");
+    try {
+      signin(email, password);
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (

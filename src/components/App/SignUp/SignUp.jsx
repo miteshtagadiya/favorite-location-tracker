93856cfd2f54e544ff14458d352ced9fac0501ff
mitesh <miteshtagadiya@gmail.com>
2020-06-13 17:00:48 +0530

    ReDesign: SignUp UI changed

--- src/components/App/SignUp/SignUp.jsx
+++ src/components/App/SignUp/SignUp.jsx
@@ -3,17 +3,19 @@ import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
+import { Link } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
 import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { signup } from "../../../helpers/auth";
+import { Paper } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
+    padding: 35,
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
@@ -45,71 +47,73 @@ export default function SignUp() {
   const handleSubmit = (event) => {
     event.preventDefault();
     setError("");
-    try {
-      signup(email, password);
-    } catch (error) {
+    signup(email, password).catch((error) => {
       setError(error.message);
-    }
+    });
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container component="main" maxWidth="sm">
       <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} onSubmit={handleSubmit} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            value={email}
-            onChange={handleChange}
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            value={password}
-            type="password"
-            onChange={handleChange}
-            id="password"
-            autoComplete="current-password"
-          />
-          {error ? (
-            <Typography className="secondary">{this.state.error}</Typography>
-          ) : null}
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link to="/login" variant="body2">
-                {"Already have an account? Sign in"}
-              </Link>
+      <Paper>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} onSubmit={handleSubmit} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              value={email}
+              onChange={handleChange}
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              value={password}
+              type="password"
+              onChange={handleChange}
+              id="password"
+              autoComplete="current-password"
+            />
+            {error ? (
+              <Typography style={{ color: "red" }} className="secondary">
+                {error}
+              </Typography>
+            ) : null}
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link to="/login" variant="body2">
+                  {"Already have an account? Sign in"}
+                </Link>
+              </Grid>
             </Grid>
-          </Grid>
-        </form>
-      </div>
+          </form>
+        </div>
+      </Paper>
     </Container>
   );
 }

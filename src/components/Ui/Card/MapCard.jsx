dedcb82dca780dc124716e9bca5eac94a6798e75
mitesh <miteshtagadiya@gmail.com>
2020-06-13 17:01:04 +0530

    Refactor: Card css refactor

--- src/components/Ui/Card/MapCard.jsx
+++ src/components/Ui/Card/MapCard.jsx
@@ -1,37 +1,30 @@
 import React from "react";
 import { Paper } from "@material-ui/core";
+import "./Card.sass";
 
 const MapCard = (props) => {
   return (
-    <Paper
-      style={{
-        position: "absolute",
-        marginTop: 50,
-        zIndex: 1,
-        marginLeft: 40,
-        width: 280,
-      }}
-    >
+    <Paper className="MapCard">
       <div style={{ padding: 15 }}>
-        <label style={{ fontSize: 16, fontWeight: "bold" }}>
+        <label className="font-bold font-label">
           {props.selectedCard.name}
         </label>
         <br />
         <label>{props.selectedCard.description}</label>
         <br />
         <label>
-          <span style={{ fontWeight: "bold" }}>latitude: </span>
+          <span className="font-bold">latitude: </span>
           {props.selectedCard.lat}
         </label>
         <br />
         <label>
-          <span style={{ fontWeight: "bold" }}>longitude: </span>
+          <span className="font-bold">longitude: </span>
           {props.selectedCard.long}
         </label>
         <br />
         <label>
           Distance between your location and {props.selectedCard.name} is{" "}
-          <span style={{ fontWeight: "bold" }}>
+          <span className="font-bold">
             {(props.distance * 0.001).toFixed(2)} Kilometers
           </span>
         </label>

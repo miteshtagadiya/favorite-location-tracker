ec759566891e632a4ebfd0dcd475a4114bdf1a39
mitesh <miteshtagadiya@gmail.com>
2020-06-26 20:22:11 +0530

    Update firebase config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,14 +3,14 @@ import "firebase/auth";
 import "firebase/database";
 
 const config = {
-  apiKey: "AIzaSyBTXgToEN4wP-iI0iG245T7sgx2KapsPGE",
-  authDomain: "speedy-solstice-280013.firebaseapp.com",
-  databaseURL: "https://speedy-solstice-280013.firebaseio.com",
-  projectId: "speedy-solstice-280013",
-  storageBucket: "speedy-solstice-280013.appspot.com",
-  messagingSenderId: "1065001118143",
-  appId: "1:1065001118143:web:f78d3f8fcea167279f190a",
-  measurementId: "G-7QVD865LCG",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER)ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 firebase.initializeApp(config);
